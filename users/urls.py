a739a7075d5c3761506b30d982625f91472676a6
isaacandhismother <vitaliboshik40@gmail.com>
2024-04-22 16:16:50 +0400

    update

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
@@ -6,6 +7,6 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.login_user, name='login'),
+    path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', views.logout_user, name='logout')
 ]
