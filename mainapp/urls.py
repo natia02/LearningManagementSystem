e6a8bcae29ccf48cec3a5a16af987e4757eb6c4a
isaacandhismother <vitaliboshik40@gmail.com>
2024-04-23 14:16:36 +0400

    update

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from mainapp.views.login import login_view
 from mainapp.views.faculty import get_faculties, get_faculty
 from mainapp.views.lecturer import get_lecturer, get_lecturers
 from mainapp.views.subject import get_subject, get_subjects
@@ -7,7 +6,6 @@ from mainapp.views.subject import get_subject, get_subjects
 
 urlpatterns = [
     path('', get_faculties, name='faculties'),
-    path('login/', login_view, name='login'),
     path('faculty/<int:faculty_id>/', get_faculty, name='faculty'),
     path('lecturer/<int:lecturer_id>', get_lecturer, name='lecturer'),
     path('lecturers/', get_lecturers, name='lecturers'),
